1be398fa8e6ca0096bf1a3c5354c63ecce3807d6
Cory House <housecor@gmail.com>
2023-06-13 06:18:30 -0500

    refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 import { Box } from "@chakra-ui/react";
-import React from "react";
 import CounterAppOne from "./components/CounterAppOne";
 
-const App = () => (
-  <Box margin="1.2rem">
-    <Box>APP-1</Box>
-    <Box>
-      <CounterAppOne />
+export function App() {
+  return (
+    <Box margin="1.2rem">
+      <Box>APP-1</Box>
+      <Box>
+        <CounterAppOne />
+      </Box>
     </Box>
-  </Box>
-);
-
-export default App;
+  );
+}
