8c75374427f45a1535d97f2e52e2c6972cca284b
Cory House <housecor@gmail.com>
2023-06-13 07:54:41 -0500

    pass props

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import { Box } from "@chakra-ui/react";
 import CounterAppOne from "./components/CounterAppOne";
+import { useNavigate } from "react-router-dom";
 
+// This component merely exists to run the remote in isolation
 export function App() {
+  const navigate = useNavigate();
+
   return (
     <Box margin="1.2rem">
       <Box>APP-1</Box>
       <Box>
-        <CounterAppOne />
+        {/* Hard coding these props for local dev of this remote in isolation. These props are provided by the shell in the production app. */}
+        <CounterAppOne parentCount={0} nav={navigate} />
       </Box>
     </Box>
   );
