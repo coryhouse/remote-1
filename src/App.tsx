75a1d0e2a3104c71dcc544b045a35240d8180e3d
Cory House <housecor@gmail.com>
2023-06-17 09:09:12 -0500

    update props

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,11 @@ export function App() {
       <Box>REMOTE-1</Box>
       <Box>
         {/* Hard coding these props for local dev of this remote in isolation. These props are provided by the shell in the production app. */}
-        <RemoteOne parentCount={0} nav={navigate} />
+        <RemoteOne
+          parentCount={0}
+          user={{ id: 2, name: "Bobby" }}
+          urls={{ about: "/about", home: "/" }}
+        />
       </Box>
     </Box>
   );
