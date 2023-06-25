2dfcbe5954b62c3b20f4befa0a43bcc68c1f0806
Cory House <housecor@gmail.com>
2023-06-25 10:54:40 -0500

    remote chakra

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,14 @@
-import { Box } from "@chakra-ui/react";
 import RemoteOne from "./components/RemoteOne";
 
 // This component merely exists to run the remote in isolation
 export function App() {
   return (
-    <Box margin="1.2rem">
-      <Box>REMOTE-1</Box>
-      <Box>
-        {/* Hard coding these props for local dev of this remote in isolation. These props are provided by the shell in the production app. */}
-        <RemoteOne
-          baseUrl=""
-          parentCount={0}
-          user={{ id: 2, name: "Bobby" }}
-          urls={{ about: "/about", home: "/" }}
-        />
-      </Box>
-    </Box>
+    <RemoteOne
+      // Hard coding these props for local dev of this remote in isolation. These props are provided by the shell in the production app. */
+      baseUrl=""
+      parentCount={0}
+      user={{ id: 2, name: "Bobby" }}
+      urls={{ about: "/about", home: "/" }}
+    />
   );
 }
