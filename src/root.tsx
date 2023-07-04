a8b58ebf6f106e6eb40655a6d449dca12901b0a0
Cory House <housecor@gmail.com>
2023-07-04 07:59:00 -0500

    call context

--- src/root.tsx
+++ src/root.tsx
@@ -1,12 +1,12 @@
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { ShellContext } from "./ShellContext";
 import RemoteOne from "./RemoteOne";
 
 // This root is used for running the remote by itself.
 // The shell wraps all remotes in a BrowserRouter, so we must do the same here.
 // The shell also passes props to the remotes, so we must do the same here.
 createRoot(document.getElementById("root") as HTMLElement).render(
-  <BrowserRouter>
+  <ShellContext>
     <RemoteOne
       baseUrl=""
       shellCount={0}
@@ -14,5 +14,5 @@ createRoot(document.getElementById("root") as HTMLElement).render(
       user={{ id: 2, name: "Cory" }}
       urls={{ about: "/about", home: "/" }}
     />
-  </BrowserRouter>
+  </ShellContext>
 );
